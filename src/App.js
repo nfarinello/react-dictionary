0c92a4093f2aae431b2bf7cfb8c3ee0927dc7eae
Nicole Farinello <nicole_mattos@live.com>
2024-05-17 14:46:18 +0100

    added the search engine to the dictionary app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       </header>
+        <main className='header'>
+          <Dictionary />
+        </main>
+        <footer className='text-center'>Coded by Nicole Farinello, this project is <a href="https://github.com/nfarinello/react-dictionary">Open-Sourced</a>.</footer>
     </div>
+  </div>
   );
 }
 
