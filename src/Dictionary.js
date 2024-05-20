23d114829d616e71d0e54e55828bf8c1a207fda9
Nicole Farinello <nicole_mattos@live.com>
2024-05-20 15:35:48 +0100

    improved the search on the CSS side

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -33,8 +33,10 @@ export default function Dictionary(props) {
   return (
     <div className="Dictionary">
         <section>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input type="search" onChange={handleKeywordChange}
+            defaultValue={props.defaultKeyword} />
           </form>
           <div className="hint">Suggested words: cat, sunrise, plans, yoga...</div>
       </section>
