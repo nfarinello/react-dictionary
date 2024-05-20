0eab4e1f3401077260f2e3d4d483bcc04d18c53b
Nicole Farinello <nicole_mattos@live.com>
2024-05-20 18:41:44 +0100

    fixed the issue with synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Synonyms from "./Synonyms";
 import "./Meaning.css";
+
 export default function Meaning(props) {
   return (
     <div className="Meaning">
@@ -8,12 +9,8 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <div className="definition">
-              {definition.definition}
-            </div>
-            <div className="example">
-              {definition.example}
-            </div>
+            <div className="definition"> {definition.definition} </div>
+            <div className="example"> {definition.example} </div>
             <Synonyms synonyms={definition.synonyms} />
             </div>
         );
