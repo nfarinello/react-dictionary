089797293e343bce6f63a581f82d394aa3aaef64
Nicole Farinello <nicole_mattos@live.com>
2024-05-20 17:20:40 +0100

    added emoji

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -46,7 +46,7 @@ export default function Dictionary(props) {
   return (
     <div className="Dictionary">
         <section>
-          <h1>What word do you want to look up?</h1>
+          <h1>What word do you want to look up?👀</h1>
           <form onSubmit={handleSubmit}>
             <input type="search" onChange={handleKeywordChange}
             defaultValue={props.defaultKeyword} />
