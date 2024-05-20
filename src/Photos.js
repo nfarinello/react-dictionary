0a72574bb4fa2464e1096c831896bf8b90853b54
Nicole Farinello <nicole_mattos@live.com>
2024-05-20 17:50:45 +0100

    fixed photo address

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props){
       {props.photos.map(function (photo, index) {
         return (
           <div className="col-4" key={index} >
-            <a href ={photo} target="_blank"rel="noreferrer" >
+            <a href ={photo.src.original} target="_blank"rel="noreferrer" >
          <img src={photo.src.landscape}
          className="img-fluid" alt="dictionary_img" />
          </a>
